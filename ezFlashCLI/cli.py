f36e53729ea52585c2a50637fad2f5bdf30e6be0
ezflash <info@ezflash.org>
2022-03-17 11:28:02 +0100

    added flashes and moved to 28beta1

--- ezFlashCLI/cli.py
+++ ezFlashCLI/cli.py
@@ -22,7 +22,7 @@
 # OR OTHER DEALINGS IN THE SOFTWARE.
 
 
-__version__ = "0.0.27"
+__version__ = "0.0.28beta1"
 
 
 import argparse
--- ezFlashCLI/ezFlash/pyjlink.py
+++ ezFlashCLI/ezFlash/pyjlink.py
@@ -285,8 +285,8 @@ class pyjlink(object):
         self.jl.JLINKARM_TIF_Select(ctypes.c_interface)
 
         self.logger.debug("Set the speed for J-Link communication with the core")
-        # c_speed = ctypes.c_uint32(self.speed)
-        # self.jl.JLINKARM_SetSpeed(c_speed)
+        c_speed = ctypes.c_uint32(self.speed)
+        self.jl.JLINKARM_SetSpeed(c_speed)
 
         self.logger.debug("Establish a connection to the target system")
         if self.jl.JLINKARM_IsConnected():
